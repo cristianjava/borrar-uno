4844b255f7b210fbacf0714e585433509a6e99d3
Cristian David Castaño Giraldo - Ceiba Software <cristian.castano@ceiba.com.co>
2018-09-11 08:04:21 -0500

    borrar

---
+++ src/main/java/com/borrar/borrar/BorrarApplication.java
@@ -0,0 +1,12 @@
+package com.borrar.borrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrarApplication.class, args);
+	}
+}
